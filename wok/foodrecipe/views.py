e8b0d2ecf0a3427f2ad4c1b1a9f250835c443b16
Nail Fatkhutdinov <liserg008@gmail.com>
2023-02-11 01:13:03 +0300

    сделал вывод всех постов

--- wok/foodrecipe/views.py
+++ wok/foodrecipe/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.views.generic import ListView
+
 from foodrecipe.models import Post
 
 
@@ -8,4 +7,8 @@ class PostsList(ListView):
     model = Post
     template_name = 'foodrecipe/index.html'
     context_object_name = 'foodrecipe_post_posts'
+    queryset = Post.objects.filter(is_published = True, moderated = True).order_by('-date_created')
 
+    #def get_queryset(self):
+    #    user = get_object_or_404(User, username = self.kwargs.get('username'))
+    #    return Post.objects.filter(author = user).order_by('-date_created')
