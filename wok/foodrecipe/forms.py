92e68ab4a6bb41da46857ab8d277f10f72ef5cfb
Nail Fatkhutdinov <liserg008@gmail.com>
2023-02-24 22:23:50 +0300

    переделал бд

--- wok/foodrecipe/forms.py
+++ wok/foodrecipe/forms.py
@@ -1,14 +1,22 @@
 from django import forms
 from .models import *
+from django.forms.models import inlineformset_factory
 
-class AddPost(forms.ModelForm):
-    # def __init__(self):
-    #     super().__init__()
-    #     self.fields['type'].empty_label = 'Не выбрано'
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'image', 'ingredients', 'type']
-        widgets = {
-            'title':forms.TextInput(attrs={'class': 'form-input'}),
-            'content':forms.TextInput(attrs={'cols': 100, 'rows':20}),
-        }
+# class AddPost(forms.ModelForm):
+#     # def __init__(self):
+#     #     super().__init__()
+#     #     self.fields['type'].empty_label = 'Не выбрано'
+#     class Meta:
+#         model = Post
+#         fields = ['title', 'content', 'image', 'ingredients', 'type']
+#         widgets = {
+#             'title':forms.TextInput(attrs={'class': 'form-input'}),
+#             'content':forms.TextInput(attrs={'cols': 100, 'rows':20}),
+#         }
+
+# class AddPost(forms.ModelForm):
+#     class Meta:
+#         model = PostContent
+#         fields = ['stage1','stage2','stage3','stage4','stage5',]
+
+class AddPost
